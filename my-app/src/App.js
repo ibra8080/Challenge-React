37940ee0134161c7bd3d6b299ccda5b5929637f9
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-07-31 15:54:26 +0000

    Add Content posts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,4 @@
+import Content from './components/Content';
 import css from './App.module.css';
 import NavBarForm from './components/NavBarForm';
 // import NavBarSimple from './components/NavBarSimple';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <Content />
       <NavBarForm />
       <Sidebar />
     </div>
